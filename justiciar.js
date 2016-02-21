2984fa2f7d9fb3995ce1332e18f5e10eed416f75
David Edwards <david.edwr@gmail.com>
2016-02-21 08:43:21 -0500

    Support focusing tests by line number

--- justiciar.js
+++ justiciar.js
@@ -1,30 +1,78 @@
 var _ = require('lodash/fp');
+var stack = require('stack-trace');
+
+function runTest(name, execute, buildContext, startLine) {
+  var filterableTest = function(endLine) {
+    return function(buildSuperContext, focusLine) {
+      if(testSuppressedByLineFocus(startLine, endLine, focusLine)) {
+        return {
+          name: name,
+          status: 'OMITTED',
+          results: []
+        };
+      } else {
+        return execute(function() { return buildContext(buildSuperContext()); }, focusLine);
+      }
+    };
+  };
+
+  filterableTest.startLine = startLine;
+  return filterableTest;
+}
 
 function describe(name, config) {
-  var buildContext = config.context || function(){};
-  var tests = config.tests || [];
+  var definedAt = stack.get()[1];
+  var buildContext = config.context || _.identity;
+  var testDefinitions = config.tests || [];
 
-  return function(superContext) {
-    function runTest(test) { return test(buildContext(superContext)); }
+  var tests = _.map(function(testPair) {
+    return testPair[0](testPair[1].startLine);
+  }, staggerredZip(testDefinitions, 'END'));
 
-    var results = _.map(runTest, tests);
+  function executeDescribe(context, focusLine) {
+    if(focusLine >= definedAt.getLineNumber() && focusLine < testDefinitions[0].startLine) {
+      focusLine = undefined;
+    }
+
+    var results = _.map(function(test) {
+      return test(context, focusLine);
+    }, tests);
 
     return {
       name: name,
+      status: aggregateStatus(results),
       results: results
     };
   }
+
+  return runTest(name, executeDescribe, buildContext, definedAt.getLineNumber());
+}
+
+function staggerredZip(array, finalElement) {
+  return _.zip(array, _.concat(_.tail(array), [finalElement]));
+}
+
+function testSuppressedByLineFocus(startLine, endLine, focusLine) {
+  return focusLine && (startLine > focusLine || (endLine !== 'END' && endLine <= focusLine));
+}
+
+function aggregateStatus(results) {
+  if(_.some(function(result) { return result === 'FAIL'; }, results)) return 'FAIL';
+  if(_.every(function(result) { return result === 'OMITTED'; }, results)) return 'OMITTED';
+  return 'PASS';
 }
 
 function it(name, test) {
-  return function(context) {
-    var result = test(context)
-
+  function executeIt(context, focusLine) {
+    var result = test(context());
     return {
       name: name,
+      status: result,
       results: [result]
     };
   }
+
+  return runTest(name, executeIt, _.identity, stack.get()[1].getLineNumber());
 }
 
 function expect(subject) {
--- run.js
+++ run.js
@@ -1,24 +1,29 @@
 var _ = require('lodash/fp');
 
-var fileToRun = process.argv[2];
+var fileAndLineToRun = _.split(':', process.argv[2]);
+var fileToRun = fileAndLineToRun[0];
+var lineToRun = fileAndLineToRun[1] ? parseInt(fileAndLineToRun[1]) : undefined;
 
-var suite = require('./' + fileToRun);
+var suite = require('./' + fileToRun)('END');
 
-var report = suite();
+var globalContext = {};
+var report = suite(function() { return globalContext; }, lineToRun);
 
 function printReport(report, indent) {
   indent = indent || 0;
   var padding = Array(indent+1).join(' ');
 
-  console.log(padding + report.name);
+  if(report.status !== 'OMITTED') {
+    console.log(padding + report.name);
 
-  _.each(function(result) {
-    if(typeof(result) === 'string') {
-      console.log(padding + '  ' + result);
-    } else {
-      printReport(result, indent+2);
-    }
-  }, report.results);
+    _.each(function(result) {
+      if(typeof(result) === 'string') {
+        console.log(padding + '  ' + result);
+      } else {
+        printReport(result, indent+2);
+      }
+    }, report.results);
+  }
 }
 
 printReport(report);
