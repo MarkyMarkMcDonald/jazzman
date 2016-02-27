a32a88b9917549980a3ed1dbec7ea0bf21fe7f64
David Edwards <david.edwr@gmail.com>
2016-02-27 14:05:43 -0500

    Extract describe module

---
+++ describe.js
@@ -0,0 +1,33 @@
+var _ = require('lodash/fp');
+var stack = require('stack-trace');
+
+var report = require('./report');
+var defineTest = require('./define-test');
+
+function staggerredZip(array, finalElement) {
+  return _.zip(array, _.concat(_.tail(array), [finalElement]));
+}
+
+module.exports = function describe(name, config) {
+  var definedAt = stack.get()[1];
+  var buildContext = config.context || _.identity;
+  var testDefinitions = config.tests || [];
+
+  var tests = _.map(function(testPair) {
+    return testPair[0](testPair[1].startLine);
+  }, staggerredZip(testDefinitions, 'END'));
+
+  function executeDescribe(context, focusLine) {
+    if(focusLine >= definedAt.getLineNumber() && focusLine < testDefinitions[0].startLine) {
+      focusLine = undefined;
+    }
+
+    var results = _.map(function(test) {
+      return test(context, focusLine);
+    }, tests);
+
+    return report(name, results);
+  }
+
+  return defineTest(name, executeDescribe, buildContext, definedAt.getLineNumber());
+}
--- justiciar.js
+++ justiciar.js
@@ -3,34 +3,7 @@ var stack = require('stack-trace');
 
 var report = require('./report');
 var defineTest = require('./define-test');
-
-function describe(name, config) {
-  var definedAt = stack.get()[1];
-  var buildContext = config.context || _.identity;
-  var testDefinitions = config.tests || [];
-
-  var tests = _.map(function(testPair) {
-    return testPair[0](testPair[1].startLine);
-  }, staggerredZip(testDefinitions, 'END'));
-
-  function executeDescribe(context, focusLine) {
-    if(focusLine >= definedAt.getLineNumber() && focusLine < testDefinitions[0].startLine) {
-      focusLine = undefined;
-    }
-
-    var results = _.map(function(test) {
-      return test(context, focusLine);
-    }, tests);
-
-    return report(name, results);
-  }
-
-  return defineTest(name, executeDescribe, buildContext, definedAt.getLineNumber());
-}
-
-function staggerredZip(array, finalElement) {
-  return _.zip(array, _.concat(_.tail(array), [finalElement]));
-}
+var describe = require('./describe');
 
 function it(name, test) {
   function executeIt(buildContext, focusLine) {
