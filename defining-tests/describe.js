bb00137f2d5c18a90d66d67c98d9d4854b19db2c
David Edwards <david.edwr@gmail.com>
2016-03-09 00:29:09 -0500

    Clean up describe module

--- defining-tests/describe.js
+++ defining-tests/describe.js
@@ -1,47 +1,58 @@
 var _ = require('lodash/fp');
+var map = _.map;
 var stack = require('stack-trace');
 
 var report = require('../report');
 var defineTest = require('./define-test');
 
 module.exports = function describe(name, config) {
-  var definedAt = stack.get()[1];
+  var startLine = stack.get()[1].getLineNumber();
   var buildContext = config.context || _.identity;
   var testDefinitions = config.tests || [];
 
-  var tests = scopeAllTestsByLineNumber(testDefinitions);
-
-  function executeDescribe(context, focusLine) {
-    if(focusLine >= definedAt.getLineNumber() && focusLine < testDefinitions[0].startLine) {
-      focusLine = undefined;
-    }
-
-    var results = _.map(function(test) {
-      return test(context, focusLine);
-    }, tests);
-
-    return report(name, results);
-  }
-
   return {
-    startLine: definedAt.getLineNumber(),
-    definition: defineTest(name, executeDescribe, buildContext)
+    startLine: startLine,
+    definition: defineTest(
+      name,
+      executeFor(name, testDefinitions, startLine),
+      buildContext
+    )
   };
 };
 
-function scopeAllTestsByLineNumber(testDefinitions) {
-  return _.map(
+function executeFor(name, testDefinitions, startLine) {
+  var firstTestStart = testDefinitions[0].startLine
+  var relevantFocus = focusLineUnlessInPreamble(startLine, firstTestStart);
+
+  return function executeDescribe(context, focusLine) {
+    var results = map(
+      function(test) { return test(context, relevantFocus(focusLine)); },
+      scopeTestsByLineNumber(testDefinitions)
+    );
+
+    return report(name, results);
+  };
+}
+
+function focusLineUnlessInPreamble(describeStart, firstTestStart) {
+  return function(focusLine) {
+    return (focusLine >= describeStart &&
+            focusLine < firstTestStart) ?
+      undefined:
+      focusLine;
+  }
+}
+
+function scopeTestsByLineNumber(testDefinitions) {
+  return map(
     _.spread(scopeTestByLineNumber),
-    staggerredZip(testDefinitions, 'END')
+    staggerredZip(testDefinitions, {startLine: 'END'})
   );
 }
 
 function scopeTestByLineNumber(test, followingTest) {
-  var definition = test.definition;
-  var startLine = test.startLine;
-  var endLine = followingTest === 'END' ? 'END' : followingTest.startLine;
-
-  return definition(startLine, endLine);
+  var endLine = followingTest.startLine;
+  return test.definition(test.startLine, endLine);
 }
 
 function staggerredZip(array, finalElement) {
